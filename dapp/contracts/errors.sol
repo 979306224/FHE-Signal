9687d1ece653f933a496a501b7ec7e5f123981eb
clkclk <979306224@qq.com>
2025-09-01 09:06:33 +0800

    refactor

--- dapp/contracts/errors.sol
+++ dapp/contracts/errors.sol
@@ -2,21 +2,3 @@
 pragma solidity ^0.8.24;
 
 error ErrDeadline();
-error ErrOnlySeller();
-error ErrStatus();
-error ErrBundle();
-error ErrSellerBid();
-error ErrNoBid();
-error ErrMinPrice();
-error ErrNativeDisabled();
-error ErrERC20Escrow();
-error ErrERC20Back();
-error ErrNotEnded();
-error ErrPayFail();
-error ErrERC20ToWinner();
-error ErrOnlyManager();
-error ErrInsufficientBalance();
-error ErrInvalidAmount();
-error ErrZeroAddress();
-
-
